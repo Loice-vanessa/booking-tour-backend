a001cca52e1b6145c578ffbee90a8ee08acba8ab
Loice-vanessa <icyezavanessa@gmail.com>
2022-06-20 10:51:24 +0200

    msg

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,15 @@
+import UserServices from "../services/userService";
+
+class UserController{
+
+    static testController(req,res){
+        const test=UserServices.testServiceFunction(req);
+        return res.status(200).json({
+            message:"Ok! Successfully ",
+            data:test
+        })
+
+    }
+
+}
+export default UserController;
---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import "dotenv/config";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+import route from "./routes";
+
+const app = express();
+app.use(bodyParser.json());
+app.use("/", route);
+// database configration
+const database = process.env.DATABASE;
+mongoose
+  .connect(database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database is well connected");
+  });
+// server configration
+const port = process.env.PORT || 4040;
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
+export default app;
